cfb902d412321341c7c84645e491c912a9f5e922
EmyLuk <emyluculescu@gmail.com>
2024-06-25 21:26:39 +0300

    mini javascript projects

---
+++ OneDrive/Desktop/WebSite-Project/Project JavaScript/Project 1/index.js
@@ -0,0 +1,13 @@
+const body=document.getElementsByTagName("body")[0]
+function setColor(name){
+    body.style.backgroundColor =name;
+}
+
+function randomColor(){
+    const red= Math.round(Math.random() *255)
+    const green= Math.round(Math.random() *255)
+    const Blue= Math.round(Math.random() *255)
+
+    const color= `rgb(${red},${green},${Blue})`
+    body.style.backgroundColor = color;
+}
